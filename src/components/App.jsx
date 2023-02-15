ea42ff769d17169d18ead3d132c7a3f0d53befb2
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-02-15 19:12:17 +0200

    update index.html

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-// import SearchFilms from "pages/films/SearchFilms";
 import { Movie } from '../pages/Movie'
 import { Home } from "pages/Home/Home";
 import { Cast } from "pages/Cast/Cast";
 import { MovieDetails } from './MovieDetails/MovieDetails'
-
-
 import { Layout } from "./Layout/Layout";
 
 export const App = () => {
@@ -15,16 +12,13 @@ export const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/movies" element={<Movie />} />
-            
             {/* <Route path="reviews" element={ <>Reviews</>} /> */}
           
           <Route path="movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast/>} />
           </Route>
         </Route>
-        {/* <Route path="/about" element={<Movies/>} /> */}
-        {/* <Route path="/products" element={<Products />} />
-        <Route path="*" element={<NotFound />} /> */}
+  
       </Routes>
     </div>
   );
